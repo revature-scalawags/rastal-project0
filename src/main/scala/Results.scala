dce60bdbcee09ad11e439207227630e608f766cf
Rastal <rastal@protonmail.com>
2020-12-18 16:35:35 -0800

    Implement writing count results to MongoDB

--- src/main/scala/Results.scala
+++ src/main/scala/Results.scala
@@ -1,8 +1,10 @@
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
 import org.mongodb.scala.bson.ObjectId
 
 object Results {
   def apply(counts: List[Count]): Results =
-    Results(new ObjectId(), counts)
+    Results(new ObjectId(), Calendar.getInstance.getTime, counts)
 }
 
-case class Results(_id: ObjectId, counts: List[Count])
+case class Results(_id: ObjectId, timestamp: Date, counts: List[Count])
